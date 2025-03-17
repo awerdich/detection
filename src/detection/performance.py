be4754c3b2fb50452b9c3539db8c7960cbfcda80
Andreas Werdich <andreas_werdich@hms.harvard.edu>
2025-03-17 16:17:51 -0400

    cumulative P and R for images

--- src/detection/performance.py
+++ src/detection/performance.py
@@ -6,49 +6,35 @@ Harvard Medical School, Boston, MA, USA
 """
 
 import numpy as np
+import torch
+from torchvision import ops
 from detection.imageproc import xywh2xyxy, xyxy2xywh, determine_bbox_format
 
-def get_iou(bbox_1: list, bbox_2: list, bbox_format: str = 'xyxy') -> float:
-    """
-    Calculates the Intersection over Union (IoU) for two bounding boxes.
-    The IoU is a measure used to quantify the overlap between two bounding
-    boxes. It is computed as the ratio of the intersection area to the union
-    area of the two bounding boxes. This function supports bounding boxes in
-    two different formats: 'xyxy' (top-left and bottom-right coordinates) or
-    'xywh' (top-left coordinates, width, and height). If the format is 'xywh',
-    the bounding boxes are converted to 'xyxy' before the calculation.
-    Args:
-        bbox_1 (list): The first bounding box provided as a list of coordinates.
-        bbox_2 (list): The second bounding box provided as a list of coordinates.
-        bbox_format (str): The format of the bounding boxes ('xyxy' or 'xywh').
-            Defaults to 'xyxy'.
-    Returns:
-        float: The Intersection over Union (IoU) value, a ratio between 0 and 1.
-    """
+def get_iou(bbox_1: list, bbox_2: list, bbox_format: str, method: str='np') -> float:
+    assert method in ['np', 'pt'], 'method should be either "np" or "pt"'
+    assert bbox_format in ['xyxy', 'xywh'], 'bbox_format should be either "xyxy" or "xywh"'
+    iou = None
     if bbox_format == 'xywh':
         bbox_1, bbox_2 = xywh2xyxy(bbox_1), xywh2xyxy(bbox_2)
-    # coordinates of the area of intersection.
-    ix1 = np.maximum(bbox_1[0], bbox_2[0])
-    iy1 = np.maximum(bbox_1[1], bbox_2[1])
-    ix2 = np.minimum(bbox_1[2], bbox_2[2])
-    iy2 = np.minimum(bbox_1[3], bbox_2[3])
-
-    # Intersection height and width.
-    i_height = np.maximum(iy2 - iy1 + 1, np.array(0.))
-    i_width = np.maximum(ix2 - ix1 + 1, np.array(0.))
-
-    area_of_intersection = i_height * i_width
-
-    # Ground Truth dimensions.
-    gt_height = bbox_1[3] - bbox_1[1] + 1
-    gt_width = bbox_1[2] - bbox_1[0] + 1
-
-    # Prediction dimensions.
-    pd_height = bbox_2[3] - bbox_2[1] + 1
-    pd_width = bbox_2[2] - bbox_2[0] + 1
-
-    area_of_union = gt_height * gt_width + pd_height * pd_width - area_of_intersection
-
-    iou = area_of_intersection / area_of_union
-
+    if method == 'np':
+        ix1 = np.maximum(bbox_1[0], bbox_2[0])
+        iy1 = np.maximum(bbox_1[1], bbox_2[1])
+        ix2 = np.minimum(bbox_1[2], bbox_2[2])
+        iy2 = np.minimum(bbox_1[3], bbox_2[3])
+        # Intersection height and width.
+        i_height = np.maximum(iy2 - iy1 + 1, np.array(0.))
+        i_width = np.maximum(ix2 - ix1 + 1, np.array(0.))
+        area_of_intersection = i_height * i_width
+        # Ground Truth dimensions.
+        gt_height = bbox_1[3] - bbox_1[1] + 1
+        gt_width = bbox_1[2] - bbox_1[0] + 1
+        # Prediction dimensions.
+        pd_height = bbox_2[3] - bbox_2[1] + 1
+        pd_width = bbox_2[2] - bbox_2[0] + 1
+        area_of_union = gt_height * gt_width + pd_height * pd_width - area_of_intersection
+        iou = area_of_intersection / area_of_union
+    elif method == 'pt':
+        bbox_tensors = [torch.tensor([bbox_1], dtype=torch.float),
+                        torch.tensor([bbox_2], dtype=torch.float)]
+        iou = ops.box_iou(bbox_tensors[0], bbox_tensors[1]).item()
     return iou
